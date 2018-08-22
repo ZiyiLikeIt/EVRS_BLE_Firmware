c65c17a38be4dce9f45c0ea2b229f84ec7acd604
Ziyi <ziyi.github@outlook.com>
2018-08-22 16:01:37 +1000

    rename other folders

---
+++ evrs_bs_stack/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/ti/ble_sdk_2_02_02_25/ble_examples/examples/cc2650lp/simple_central/ccs/config/lib_linker.cmd" -l"C:/ti/ble_sdk_2_02_02_25/src/rom/enc_lib/cc26xx_ecc_rom_api.a" -l"C:/ti/ble_sdk_2_02_02_25/src/rom/ble_rom_releases/04242014/ble_rom_patch.symbols" -l"C:/ti/tirtos_cc13xx_cc26xx_2_21_01_08/products/cc26xxware_2_24_03_17272/driverlib/bin/ccs/driverlib.lib" -l"C:/ti/ble_sdk_2_02_02_25/src/rom/enc_lib/cc26xx_ecc_rom_api.a" -llibc.a
+
---
+++ evrs_tx_cc2650lp_app/FlashROM/src/simple_gatt_profile.d
@@ -0,0 +1,63 @@
+# FIXED
+
+src/simple_gatt_profile.obj: ../src/simple_gatt_profile.c
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/bcomdef.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/comdef.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/hal_types.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/../_common/cc26xx/_hal_types.h
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/inc/hal_defs.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/osal.h
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/limits.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/OSAL_Memory.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/OSAL_Timers.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/icall/src/inc/ICall.h
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+src/simple_gatt_profile.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/inc/hal_assert.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/hal_types.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/linkdb.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/att.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/l2cap.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/gatt.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/gatt_uuid.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/gattservapp.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/profiles/roles/gapbondmgr.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/gap.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/sm.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/inc/hci.h
+src/simple_gatt_profile.obj: C:/ti/ble_sdk_2_02_02_25/src/controller/cc26xx/inc/ll.h
+src/simple_gatt_profile.obj: ../src/simple_gatt_profile.h
+
+../src/simple_gatt_profile.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/bcomdef.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/comdef.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/hal_types.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/../_common/cc26xx/_hal_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/inc/hal_defs.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/osal.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/limits.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/OSAL_Memory.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/osal/src/inc/OSAL_Timers.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/icall/src/inc/ICall.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/inc/hal_assert.h: 
+C:/ti/ble_sdk_2_02_02_25/src/components/hal/src/target/_common/hal_types.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/linkdb.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/att.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/l2cap.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/gatt.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/gatt_uuid.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/gattservapp.h: 
+C:/ti/ble_sdk_2_02_02_25/src/profiles/roles/gapbondmgr.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/gap.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/sm.h: 
+C:/ti/ble_sdk_2_02_02_25/src/inc/hci.h: 
+C:/ti/ble_sdk_2_02_02_25/src/controller/cc26xx/inc/ll.h: 
+../src/simple_gatt_profile.h: 
